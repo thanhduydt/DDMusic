601d405b12c61a2492f6f458f3a8ac2f76dfef61
kydat9446 <73746848+kydat9446@users.noreply.github.com>
2021-05-14 12:47:50 +0700

    Chinh header va them trang ca si

--- DDMusic/Controllers/HomeController.cs
+++ DDMusic/Controllers/HomeController.cs
@@ -43,6 +43,10 @@ namespace DDMusic.Controllers
         {
             return View();
         }
+        public IActionResult Singer()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
