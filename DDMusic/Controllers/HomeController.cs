dbecfa769f3e270a79d7b357e6267cafbd733733
thanhduydt <0306181301@caothang.edu.vn>
2021-05-15 13:14:49 +0700

    Them trang PersonalPage

--- DDMusic/Controllers/HomeController.cs
+++ DDMusic/Controllers/HomeController.cs
@@ -47,6 +47,10 @@ namespace DDMusic.Controllers
         {
             return View();
         }
+        public IActionResult PersonalPage()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
