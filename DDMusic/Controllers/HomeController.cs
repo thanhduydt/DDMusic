9a8d5d9f39e681df978cbb5fbf34c15159849836
kydat9446 <0306181306@caothang.edu.vn>
2021-05-23 13:51:28 +0700

    Thêm View Contact

--- DDMusic/Controllers/HomeController.cs
+++ DDMusic/Controllers/HomeController.cs
@@ -63,6 +63,10 @@ namespace DDMusic.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
