9a9e4d092dc95c44d345ac67a4c335d5b6c46229
kydat9446 <0306181306@caothang.edu.vn>
2021-05-29 17:31:59 +0700

    Them view quan ly bai hat và chinh giao dien frontend

---
+++ DDMusic/Areas/Admin/Controllers/SongController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDMusic.Areas.Admin.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateSong()
+        {
+            return View();
+        }
+        public IActionResult EditSong()
+        {
+            return View();
+        }
+    }
+}
