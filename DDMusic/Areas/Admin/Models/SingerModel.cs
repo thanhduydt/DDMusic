0ffb6c6d14d5fddb663493c33be0f5a7dc62c282
kydat9446 <0306181306@caothang.edu.vn>
2021-06-12 15:31:01 +0700

    them view singer, album, playlist

---
+++ DDMusic/Areas/Admin/Models/SingerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDMusic.Areas.Admin.Models
+{
+    public class SingerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DDMusic/Areas/Admin/Models/SongModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDMusic.Areas.Admin.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Gerne { get; set; }
+        public int Singer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Users { get; set; }
+        public string Lyric { get; set; }
+        public bool Accept { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
