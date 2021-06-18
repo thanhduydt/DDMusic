920cbceef8a2a14289b54a218560bf8fc01243ea
kydat9446 <0306181306@caothang.edu.vn>
2021-06-18 18:03:58 +0700

    Hoàn thành chức năng thêm xóa sửa của Song

--- DDMusic/Areas/Admin/Controllers/SongController.cs
+++ DDMusic/Areas/Admin/Controllers/SongController.cs
@@ -7,52 +7,169 @@ using System.IO;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using DDMusic.Areas.Admin.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DDMusic.Areas.Admin.Controllers
 {
     public class SongController : Controller
     {
-        //private readonly DPContext _context;
+        private readonly DPContext _context;
 
-        //public ProductsController(DPContext context)
-        //{
-        //    _context = context;
-        //}
-
-        public IActionResult Index()
+        public SongController(DPContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Song = _context.Song.Include(s => s.Singers);
+            return View(await Song.ToListAsync());
         }
         public IActionResult CreateSong()
         {
-            ViewBag.ListGerne = Gerne.GetAllGerne();
+            ViewData["ListGenre"] = new SelectList(SongModel.GetAllGerne());
+            ViewData["IdSinger"] = new SelectList(_context.Singer, "Id", "Name");
             return View();
         }
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,Name,Image,Price,Description,Status,Catid")] SongModel song, IFormFile ful)
-        ////{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(song);
-        //        await _context.SaveChangesAsync();
-        //        var path = Path.Combine(
-        //            Directory.GetCurrentDirectory(), "wwwroot/images/product", product.Id + "." + ful.FileName.Split(".")[ful.FileName.Split(".").Length - 1]);
-        //        using (var stream = new FileStream(path, FileMode.Create))
-        //        {
-        //            await ful.CopyToAsync(stream);
-        //        }
-        //        product.Image = product.Id + "." + ful.FileName.Split(".")[ful.FileName.Split(".").Length - 1];
-        //        _context.Update(product);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["Catid"] = new SelectList(_context.typeProduct, "Id", "Id", product.Catid);
-        //    return View(product);
-        //}
-        public IActionResult EditSong()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,IdSinger,ReleaseDate,URLImg,URLMusic,Genre,Lyric")] SongModel song, IFormFile ful, IFormFile fulMusic)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(song);
+                await _context.SaveChangesAsync();
+                if (ful != null)
+                {
+                    var path = Path.Combine(
+                    Directory.GetCurrentDirectory(), "wwwroot/img/song", song.Id + "." + ful.FileName.Split(".")[ful.FileName.Split(".").Length - 1]);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await ful.CopyToAsync(stream);
+                    }
+                    song.URLImg = song.Id + "." + ful.FileName.Split(".")[ful.FileName.Split(".").Length - 1];
+                    _context.Update(song);
+                    await _context.SaveChangesAsync();
+
+                }
+                if (fulMusic != null)
+                {
+                    //Bỏ dấu
+                    string NameMusic = RemoveUnicode(song.Name);
+                    //Bỏ khoảng cách
+                    NameMusic = NameMusic.Replace(" ", String.Empty);
+                    var path = Path.Combine(
+                    Directory.GetCurrentDirectory(), "wwwroot/audio", NameMusic + "." + fulMusic.FileName.Split(".")[fulMusic.FileName.Split(".").Length - 1]);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await ful.CopyToAsync(stream);
+                    }
+                    song.URLMusic = NameMusic + "." + fulMusic.FileName.Split(".")[fulMusic.FileName.Split(".").Length - 1];
+                    _context.Update(song);
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdSinger"] = new SelectList(_context.Singer, "Id", "Name");
+            ViewData["ListGenre"] = new SelectList(SongModel.GetAllGerne());
+            ViewBag.Alert = "Tạo mới bài hát không thành công, vui lòng thử lại";
+            return View(song);
+        }
+        public async Task<IActionResult> EditSong(int id)
+        {
+            ViewData["IdSinger"] = new SelectList(_context.Singer, "Id", "Name");
+            ViewData["ListGenre"] = new SelectList(SongModel.GetAllGerne());
+            var song = await _context.Song.FindAsync(id);
+            return View(song);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,IdSinger,ReleaseDate,URLImg,URLMusic,Genre,Lyric")] SongModel song, IFormFile ful)
+        {
+            if (id != song.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (ful != null)
+                    {
+                        string t = song.Id + "." + ful.FileName.Split(".")[ful.FileName.Split(".").Length - 1];
+                        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/song", song.URLImg);
+                        if (System.IO.File.Exists(path))
+                        {
+                            System.IO.File.Delete(path);
+                        }
+                        path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/song", t);
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await ful.CopyToAsync(stream);
+                        }
+                        song.URLImg = t;
+                        _context.Update(song);
+                        await _context.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        _context.Update(song);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SongExists(song.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(song);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var SongModel = await _context.Song.FindAsync(id);
+            _context.Song.Remove(SongModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool SongExists(int id)
+        {
+            return _context.Song.Any(e => e.Id == id);
+        }
+
+
+
+
+        public static string RemoveUnicode(string text)
+        {
+            string[] arr1 = new string[] { "á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ",
+                                            "đ",
+                                            "é","è","ẻ","ẽ","ẹ","ê","ế","ề","ể","ễ","ệ",
+                                            "í","ì","ỉ","ĩ","ị",
+                                            "ó","ò","ỏ","õ","ọ","ô","ố","ồ","ổ","ỗ","ộ","ơ","ớ","ờ","ở","ỡ","ợ",
+                                            "ú","ù","ủ","ũ","ụ","ư","ứ","ừ","ử","ữ","ự",
+                                            "ý","ỳ","ỷ","ỹ","ỵ",};
+            string[] arr2 = new string[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
+                                            "d",
+                                            "e","e","e","e","e","e","e","e","e","e","e",
+                                            "i","i","i","i","i",
+                                            "o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o",
+                                            "u","u","u","u","u","u","u","u","u","u","u",
+                                            "y","y","y","y","y",};
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                text = text.Replace(arr1[i], arr2[i]);
+                text = text.Replace(arr1[i].ToUpper(), arr2[i].ToUpper());
+            }
+            return text;
         }
     }
 }
--- DDMusic/Areas/Admin/Models/SongModel.cs
+++ DDMusic/Areas/Admin/Models/SongModel.cs
@@ -10,11 +10,9 @@ namespace DDMusic.Areas.Admin.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Gerne { get; set; }
-        [ForeignKey("IdSinger")]
+        public string Genre { get; set; }
         public int IdSinger { get; set; }
         public DateTime ReleaseDate { get; set; }
-        [ForeignKey("IdUser")]
         public string IdUser { get; set; }
         public string Lyric { get; set; }
         public string URLImg { get; set; }
@@ -22,21 +20,16 @@ namespace DDMusic.Areas.Admin.Models
         public int CountView { get; set; }
         public bool Accept { get; set; }
         public bool IsActive { get; set; }
-        //public virtual List<SingerModel> Singers { get; set; }
-        //public virtual UserModel User { get; set; }
-    }
-    public class Gerne
-    {
-        public int Id { get; set; }
-        public string GerneName { get; set; }
+        [ForeignKey("IdSinger")]
+        public virtual SingerModel Singers { get; set; }
+        [ForeignKey("IdUser")]
+        public virtual UserModel User { get; set; }
 
-        public static List<Gerne> GetAllGerne()
+        public static List<string> GetAllGerne()
         {
-            List<Gerne> gernes = new List<Gerne>()
+            List<string> gernes = new List<string>()
             {
-                new Gerne{Id=1,GerneName="Acoustic"},
-                new Gerne{Id=2,GerneName="EDM"},
-                new Gerne{Id=3, GerneName="Pop"}
+                "Acoustic","EDM","Pop","Ballad"
             };
             return gernes;
         }
