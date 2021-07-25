b2de257eda5c60d6c6dff19a68b93cc1a4e159ed
kydat9446 <0306181306@caothang.edu.vn>
2021-07-25 20:06:33 +0700

    - Thêm chức năng sửa, xóa playlist - Thêm bài hát xóa bài hát trong playlist

--- DDMusic/Areas/Admin/Controllers/PlaylistController.cs
+++ DDMusic/Areas/Admin/Controllers/PlaylistController.cs
@@ -67,5 +67,121 @@ namespace DDMusic.Areas.Admin.Controllers
             ViewData["IdSong"] = new SelectList(_context.Song, "Id", "Name");
             return View("CreatePlaylist");
         }
+        public IActionResult EditPlaylist(int id)
+        {
+            //Tìm playlist 
+            var playlist = _context.Playlist.Find(id);
+            return View(playlist);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Image")] Playlist model, IFormFile ful)
+        {
+            if (id != 0)
+            {
+
+                return View("EditPlaylist", model);
+
+            }
+            if (ModelState.IsValid)
+            {
+                if (ful != null)
+                {
+                    string t = model.Id + "." + ful.FileName.Split(".")[ful.FileName.Split(".").Length - 1];
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/playlist", model.Image);
+                    if (System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Delete(path);
+                    }
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/song", t);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await ful.CopyToAsync(stream);
+                    }
+                    model.Image = t;
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View("EditPlaylist", model);
+        }
+        public IActionResult DetailPlaylist(int id)
+        {
+            var playlist = _context.Playlist.Find(id);
+            ViewBag.NamePlaylist = playlist.Name;
+            ViewBag.IdPlaylist = playlist.Id;
+            return View();
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var Playlist = await _context.Playlist.FindAsync(id);
+            _context.Playlist.Remove(Playlist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetSongFromPlaylist(int id)
+        {
+            var Song = _context.Song;
+            var DetailPlaylist = _context.PlaylistDetail.Where(m => m.IdPlaylist == id).ToList();
+            List<SongModel> SongFromPlaylist = new List<SongModel>();
+            foreach (var item in DetailPlaylist)
+            {
+                SongModel songModel = new SongModel();
+                songModel = _context.Song.Find(item.IdSong);
+                var singer = _context.Singer.Find(songModel.IdSinger);
+                songModel.Singer = singer;
+                SongFromPlaylist.Add(songModel);
+            }
+
+            ViewData["IdSong"] = new SelectList(_context.Song, "Id", "Name");
+            ViewBag.IdPlaylist = id;
+            return PartialView("_GetSongFromPlaylist", SongFromPlaylist);
+        }
+        [HttpPost]
+        public async Task<IActionResult> InsertSongToPlaylist(int IdPlaylist, List<int> IdSong)
+        {
+            foreach (var item in IdSong)
+            {
+                var PlaylistDetailExist = _context.PlaylistDetail.Where(m => m.IdSong == item).ToList();
+                if (PlaylistDetailExist.Count == 0)
+                {
+                    PlaylistDetail playlistDetail = new PlaylistDetail();
+                    playlistDetail.IdPlaylist = IdPlaylist;
+                    playlistDetail.IdSong = item;
+                    _context.Add(playlistDetail);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return RedirectToAction("DetailPlaylist", "Playlist", new { area = "Admin", id = IdPlaylist });
+        }
+        [HttpPost]
+        public async Task<IActionResult> RemoveSongFromPlaylist(int idSong, int idPlaylist)
+        {
+            var PlaylistDetail = _context.PlaylistDetail.Where(m => m.IdSong == idSong).ToList();
+            _context.PlaylistDetail.Remove(PlaylistDetail[0]);
+            await _context.SaveChangesAsync();
+
+
+            var DetailPlaylist = _context.PlaylistDetail.Where(m => m.IdPlaylist == idPlaylist).ToList();
+            List<SongModel> SongFromPlaylist = new List<SongModel>();
+            foreach (var item in DetailPlaylist)
+            {
+                SongModel songModel = new SongModel();
+                songModel = _context.Song.Find(item.IdSong);
+                var singer = _context.Singer.Find(songModel.IdSinger);
+                songModel.Singer = singer;
+                SongFromPlaylist.Add(songModel);
+            }
+            // return PartialView("_GetSongFromPlaylist", SongFromPlaylist);
+            return RedirectToAction("DetailPlaylist", "Playlist", new { area = "Admin", id = idPlaylist });
+        }
     }
 }
